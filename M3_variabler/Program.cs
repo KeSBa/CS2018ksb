1b57f6d9bd24b3d71ddff66fd971719743beaba9
Kristian Schmidt Bertelsen <kristianbertelsen@gmail.com>
2018-05-14 13:59:01 +0200

    stylecop

--- M3_variabler/Program.cs
+++ M3_variabler/Program.cs
@@ -10,6 +10,37 @@ namespace M3_variabler
     {
         static void Main(string[] args)
         {
+            System.Int32 i = 0;
+            int x;
+
+            string tmp = i.ToString();
+
+            bool t = false;
+            System.Boolean b = true;
+
+            double k = 1234444.233;
+            Console.WriteLine(k.ToString("N3"));
+
+            int e1;
+            double e2;
+            bool e3;
+
+            System.DateTime d = DateTime.MinValue;
+            DateTime d2 = DateTime.Now;
+            DateTime d3 = new DateTime(2018, 12, 24);
+
+            Console.WriteLine(d3.ToString("yyyy dddd"));
+
+            TimeSpan ts1 = d3.Subtract(d2);
+            Console.WriteLine(ts1.TotalMinutes);
+
+            TimeSpan ts2 = new TimeSpan(14, 00, 00);
+            Console.WriteLine(ts2);
+
+             
+
+           
+
         }
     }
 }
