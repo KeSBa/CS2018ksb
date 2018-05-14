0107c925f9103b8089a99d0e3ecc2149cb58cfde
Kristian Schmidt Bertelsen <kristianbertelsen@gmail.com>
2018-05-14 12:45:07 +0200

    2

---
+++ DebugDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+
+
+            if(System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("Tryk!");
+                Console.ReadKey();
+            }
+
+            
+
+        }
+    }
+}
