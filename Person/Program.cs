a37d788fddffaa0c1f39c0763f5a062331940dfc
Kristian Schmidt Bertelsen <kristianbertelsen@gmail.com>
2018-05-15 16:09:46 +0200

    End of day 2.

---
+++ Person/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Efternavn = "Hansen";
+            p1.Fornavn = "Per";
+            p1.Fødselsår = 1934;
+
+            int Alder = p1.EstimatedLife();
+            Console.WriteLine(Alder);
+            Console.ReadKey();
+        }
+
+        internal class Person
+        {
+            public string Fornavn;
+            public string Efternavn;
+            public int Fødselsår;
+
+
+            public string FuldtNavn()
+            {
+                return $"{Fornavn} {Efternavn}";
+            }
+
+            public int EstimatedLife()
+            {
+                int y = DateTime.Now.Year;
+                return y - this.Fødselsår;
+            }
+            //default constructor
+            public Person()
+            {
+                this.Fornavn = "";
+                this.Efternavn = "";
+
+            }
+
+            //Custom constructor // chained 
+            public Person(string efternavn, string fornavn, int fødselsår) :this()
+            {
+                this.Fødselsår = fødselsår;
+                this.Fornavn = fornavn.ToLower();
+                this.Efternavn = efternavn.ToUpper();
+
+            }
+        }
+    }
+}
